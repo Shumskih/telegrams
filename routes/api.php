023cce163af7e2aeb7562c643e437ee1a66eeeab
Aleksander <alex.shumskih@gmail.com>
2019-10-20 18:17:37 +0300

    Fixed path to MessageCcontroller

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('bot/send-message', 'MessageController@sendMessage');
+Route::post('bot/send-message', 'Api\MessageController@sendMessage');
